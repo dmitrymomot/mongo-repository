12d099ba18da7b622454ac6255178593b40c70e8
Dmitry Momot <mail@dmomot.com>
2024-01-20 00:45:47 +0200

    Add predefined errors to mongorepository package

---
+++ errors.go
@@ -0,0 +1,19 @@
+package mongorepository
+
+import "errors"
+
+// Predefined errors
+var (
+	ErrNotFound                 = errors.New("document not found")
+	ErrFailedToFindByID         = errors.New("failed to find document by id")
+	ErrFailedToFindByIDs        = errors.New("failed to find documents by ids")
+	ErrInvalidDocumentID        = errors.New("invalid document id")
+	ErrFailedToCreate           = errors.New("failed to create document")
+	ErrFailedToUpdate           = errors.New("failed to update document")
+	ErrFailedToUpdateMany       = errors.New("failed to update documents")
+	ErrFailedToDelete           = errors.New("failed to delete document")
+	ErrFailedToFindOneByFilter  = errors.New("failed to find a document by the given filter")
+	ErrFailedToFindManyByFilter = errors.New("failed to find any documents by the given filter")
+	ErrFailedToCreateIndex      = errors.New("failed to create collection index")
+	ErrFailedToDeleteMany       = errors.New("failed to delete documents")
+)
